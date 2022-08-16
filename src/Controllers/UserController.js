89ab21dee862d0ddfb7b5c54c245781d4e2268cb
Vander-Reis <vanderreis2017@outlook.com>
2022-08-16 15:30:08 -0300

    create controller users

--- src/Controllers/UserController.js
+++ src/Controllers/UserController.js
@@ -1,20 +1,27 @@
+const { hash } = require("bcryptjs");
 const AppError = require("../utils/ErroApp");
+const Knex = require("../database/knex");
 
 class UserController {
 
-    create( request, response) {
+    async create( request, response) {
 
         const { name, email, password } = request.body;
 
-        if(!name) {
-            throw new AppError("Favor inserir o seu nome")
-        };
+        // verificar se o e-mail já existe
+        const checkUserExists = await Knex("users").where({ email }).first();
 
-        response.status(201).json({
-            name,
-            email,
-            password
-        });
+        console.log(checkUserExists)
+
+        if(checkUserExists) {
+            throw new AppError("Este e-mail já está em uso.");
+        }
+
+        const hashPassword = await hash(password, 8);
+
+        await Knex("users").insert({ name, email, password: hashPassword});
+
+        return response.status(201).json();
 
     }
 
