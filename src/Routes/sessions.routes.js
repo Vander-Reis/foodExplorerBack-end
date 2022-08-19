17fbb6074c78780e3d786633151715db6859bb76
Vander-Reis <vanderreis2017@outlook.com>
2022-08-19 18:27:57 -0300

    configs

--- src/Routes/sessions.routes.js
+++ src/Routes/sessions.routes.js
@@ -1,6 +1,6 @@
 const { Router } = require("express");
 
-const SessionsController = require("../controllers/SessionsController");
+const SessionsController = require("../Controllers/SessionsController");
 
 const sessionsRoutes = Router();
 
